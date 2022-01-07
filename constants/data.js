a67be8dc068c8aa7443290ec45ca694de8012a08
Ettore Foti <ettorefoti9@gmail.com>
2022-01-07 13:37:51 +0100

    WIP update

---
+++ constants/data.js
@@ -0,0 +1,82 @@
+const userData = {
+  githubUsername: "ettorefoti",
+  name: "Ettore Foti",
+  designation: "DevOps Engineer",
+  avatarUrl: "/avatar2.png",
+  email: "ettorefoti9@gmail.com",
+  phone: "+39 3493018310",
+  address: "Acireale, Catania, Italy.",
+  projects: [
+    {
+      title: "Ansible Kubeadm Init and Configure",
+      link: "https://github.com/EttoreFoti/Ansible-K8s",
+      imgUrl: "/ansible.png",
+    },
+  ],
+  about: {
+    title:
+      "I'm a DevOps that loves challenges and always learning new technologies",
+    description: [
+      `I've been playing around with Linux and OS level technology since I was 13 years old. I started working as Helpdesk technician a few years ago and from there on I never stopped learning and building up both my skills and m carreer.`,
+      `After learning JavaScript, NodeJS and using Typescript, I understood that being a full time coder wasn't for me and that iI was really attracted about the entire architecture behind the code, so I started toying around with Docker and luckily enough I had the chance of using it in our production environment after only 6 months.`,
+      `Now I'm a DevOps Engineer at Fincons and working as a freelance on different projects collaborating on the foundation of a clean development workflow to allow correct use of Version Control, Microservices Architecture and CI/CD Pipelines.`,
+    ],
+    currentProject: "Ansible Kubeadm Init and Configure",
+    currentProjectUrl: "https://github.com/EttoreFoti/Ansible-K8s",
+  },
+  experience: [
+    {
+      title: "DevOps Engineer",
+      company: "https://www.finconsgroup.com",
+      year: "2022",
+      companyLink: "https://movia.biz",
+      desc: "New role, new challenge, finally hired to do what I really love.",
+    },
+    {
+      title: "Backend Developer (NodeJS)",
+      company: "Movia Spa",
+      year: "2021",
+      companyLink: "https://movia.biz",
+      desc: "I always took tasks when i had free time from the Lead Developer to always keep learning and finding what i like the most. Here I understood that I'm able to fill this role but that's not what I love doing, working on the infrastructure is.",
+    },
+    {
+      title: "L2 HelpDesk/Linux System Administrator",
+      company: "Movia Spa",
+      year: "2021",
+      companyLink: "https://movia.biz",
+      desc: "Thanks to my eager to learn, I was able to start wokimg on server and having other people escalate problems to me. That's the time where I learnt working with Docker and CI/CD pipelines.",
+    },
+    {
+      title: "CompTIA Security - Certfied Kubernetes Administrator",
+      company: "CompTIA, The Linux Foundation",
+      year: "2021",
+      companyLink: "https://training.linuxfoundation.org",
+      desc: "I love using certs to discover new skills, I find studying for certs is the best way to start learning a new technology ì, plus it' funny to take and test and goes on the resume.",
+    },
+    {
+      title: "L1 HelpDesk",
+      company: "Movia Spa",
+      year: "2020",
+      companyLink: "https://movia.biz",
+      desc: "I got hired for my first IT job, started from the ground and always asked for what I didn't know, learnt a lot and moved on after only 2 months.",
+    },
+    {
+      title: "High School",
+      company: "Liceo Scientifico Archimede, Acireale",
+      year: "2015",
+      companyLink: "https://www.liceoarchimede.edu.it",
+      desc: "Subjects being PCM - I barely survived with 93.6% marks aggregate. Flex fridays, fellas.",
+    },
+  ],
+  resumeUrl:
+    "https://drive.google.com/file/d/1xmE3BOmgM7TAOOgVp36xQIQvYDntDYoo/view?usp=sharing",
+  socialLinks: {
+    instagram: "https://www.instagram.com/ettorefoti/",
+    twitter: "https://twitter.com/",
+    linkedin: "https://linkedin.com/in/ettore-foti-188b73199",
+    github: "https://github.com/EttoreFoti",
+    facebook: "https://facebook.com",
+  },
+};
+
+export default userData;
